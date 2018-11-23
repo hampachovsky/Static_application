5b3966e1eae6b8f7bbd7ebede64fe12e42554df2
Hampachovsky <hampe1324@gmail.com>
2018-11-23 21:42:36 +0200

    add Slider_modall

---
+++ Slider_Modal_Project/src/js/script.js
@@ -0,0 +1,57 @@
+const on_Title_Slide_Click_Open_Modal = document.getElementsByClassName("Title-Slide-Click-Open-Modal");
+let Current_Slide = 0; 
+
+Show_Slide(); //Show first slide
+function Change_Slide(change_slide) {
+	Current_Slide += change_slide; //on click + or - slide
+	Show_Slide(); 
+}
+
+
+
+function Show_Slide () {
+	const Slide = document.getElementsByClassName('Slide'); 
+	for (let i = 0; i < Slide.length; i++) {
+		Slide[i].style.display = "none"; 
+	}
+
+	if(Current_Slide >= Slide.length) { 
+		Current_Slide = 0;
+	}
+	if (Current_Slide <= 0) {
+		Current_Slide = 0;
+	}
+	for (let index = 0; index < on_Title_Slide_Click_Open_Modal.length; index++) {
+		on_Title_Slide_Click_Open_Modal[index].style.color = "rgb(255,255,255)"; // change to default color 
+		
+	}	
+	//Slide[Current_Slide].style.display = "flex"
+	$(Slide[Current_Slide]).fadeIn("fast");
+}
+
+const modal_alert = document.getElementsByClassName("modal-alert"); 
+const btn_close_modal = document.getElementsByClassName("btn-close");
+
+
+
+
+
+for (let index = 0; index < on_Title_Slide_Click_Open_Modal.length; index++) {
+	on_Title_Slide_Click_Open_Modal[index].addEventListener("click", (event) => {
+		event.target.style.color = "rgb(34, 34, 34)"; //Click on title slide change color
+		$(modal_alert[index]).show("slow"); // show modal
+		modal_alert[index].style.display = "flex"; // animete showing
+			
+		});
+	
+	btn_close_modal[index].addEventListener("click", () => { 
+		$(modal_alert[index]).fadeOut("slow"); //hide when click on btn close
+	});
+	
+	
+}
+
+
+
+
+
