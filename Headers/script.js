b0f932a07b4156e5370a62ffb1e92bf8d5f00572
hampachovsky <45235492+hampachovsky@users.noreply.github.com>
2019-03-01 21:25:22 +0200

    add remove func

--- Headers/script.js
+++ Headers/script.js
@@ -1,25 +1,41 @@
-const input = document.querySelector("input");
+const headerRange = document.querySelector("#range-header");
 const header = document.querySelector("h1");
-bindValue();
+const gapRange = document.querySelector("#gap-range");
+const headerRangeLabel = document.querySelectorAll("label")[0];
+const gapRangeLabel = document.querySelectorAll("label")[1];
 
-function bindValue() {
-    const label = document.querySelector("label");
+function bindValue(label, input) {
     label.innerText = `${input.value}px`;
 }
 
-function resetValue(standartValue) {
-    input.value = standartValue;
-    bindValue();
+function resetValue(standartHeaderValue, standartGapValue) {
+    headerRange.value = standartHeaderValue;
+    gapRange.value = standartGapValue;
+    header.style.fontSize = `${standartHeaderValue}px`;
+    header.style.margin = `${standartGapValue}px`;
+    bindValue(headerRangeLabel, headerRange);
+    bindValue(gapRangeLabel, gapRange);
 }
 
-input.addEventListener("change", event => {
-    bindValue();
+gapRange.addEventListener("change", event => {
+    bindValue(gapRangeLabel, gapRange);
+    header.style.margin = `${event.target.value}px`;
+});
 
+headerRange.addEventListener("change", event => {
+    bindValue(headerRangeLabel, headerRange);
     header.style.fontSize = `${event.target.value}px`;
 });
 
 const resetBtn = document.querySelector(".btn");
 
 resetBtn.addEventListener("click", () => {
-    resetValue(32);
+    resetValue(32, 10);
+});
+
+const container = document.querySelector(".container");
+const removeBtn = document.querySelector(".remove-btn");
+
+removeBtn.addEventListener("click", () => {
+    container.remove();
 });
