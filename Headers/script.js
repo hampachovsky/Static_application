8331ea9153ebac77907aec7a5dfbe8559221d36d
Hampachovsky <hampe1324@gmail.com>
2019-03-07 20:13:24 +0200

    add eslint

--- Headers/script.js
+++ Headers/script.js
@@ -1,41 +1,42 @@
-const headerRange = document.querySelector("#range-header");
-const header = document.querySelector("h1");
-const gapRange = document.querySelector("#gap-range");
-const headerRangeLabel = document.querySelectorAll("label")[0];
-const gapRangeLabel = document.querySelectorAll("label")[1];
+const headerRange = document.querySelector('#range-header');
+const header = document.querySelector('h1');
+const gapRange = document.querySelector('#gap-range');
+const headerRangeLabel = document.querySelectorAll('label')[0];
+const gapRangeLabel = document.querySelectorAll('label')[1];
 
 function bindValue(label, input) {
-    label.innerText = `${input.value}px`;
+  const argumentsLabel = label;
+  argumentsLabel.innerText = `${input.value}px`;
 }
 
 function resetValue(standartHeaderValue, standartGapValue) {
-    headerRange.value = standartHeaderValue;
-    gapRange.value = standartGapValue;
-    header.style.fontSize = `${headerRange.value}px`;
-    header.style.margin = `${gapRange.value}px`;
-    bindValue(headerRangeLabel, headerRange);
-    bindValue(gapRangeLabel, gapRange);
+  headerRange.value = standartHeaderValue;
+  gapRange.value = standartGapValue;
+  header.style.fontSize = `${headerRange.value}px`;
+  header.style.margin = `${gapRange.value}px`;
+  bindValue(headerRangeLabel, headerRange);
+  bindValue(gapRangeLabel, gapRange);
 }
 
-gapRange.addEventListener("change", event => {
-    bindValue(gapRangeLabel, gapRange);
-    header.style.margin = `${event.target.value}px`;
+gapRange.addEventListener('change', (event) => {
+  bindValue(gapRangeLabel, gapRange);
+  header.style.margin = `${event.target.value}px`;
 });
 
-headerRange.addEventListener("change", event => {
-    bindValue(headerRangeLabel, headerRange);
-    header.style.fontSize = `${event.target.value}px`;
+headerRange.addEventListener('change', (event) => {
+  bindValue(headerRangeLabel, headerRange);
+  header.style.fontSize = `${event.target.value}px`;
 });
 
-const resetBtn = document.querySelector(".btn");
+const resetBtn = document.querySelector('.btn');
 
-resetBtn.addEventListener("click", () => {
-    resetValue(32, 10);
+resetBtn.addEventListener('click', () => {
+  resetValue(32, 10);
 });
 
-const container = document.querySelector(".container");
-const removeBtn = document.querySelector(".remove-btn");
+const container = document.querySelector('.container');
+const removeBtn = document.querySelector('.remove-btn');
 
-removeBtn.addEventListener("click", () => {
-    container.remove();
+removeBtn.addEventListener('click', () => {
+  container.remove();
 });
--- Slider_Modal_Project/src/js/script.js
+++ Slider_Modal_Project/src/js/script.js
@@ -1,41 +1,44 @@
-const on_Title_Slide_Click_Open_Modal = document.getElementsByClassName("Title-Slide-Click-Open-Modal");
-let Current_Slide = 0; 
+const on_Title_Slide_Click_Open_Modal = document.getElementsByClassName(
+    "Title-Slide-Click-Open-Modal"
+);
+let Current_Slide = 0;
 
 Show_Slide(); //Show first slide
 function Change_Slide(change_slide) {
-	Current_Slide += change_slide; //on click + or - slide
-	Show_Slide(); 
+    Current_Slide += change_slide; //on click + or - slide
+    Show_Slide();
 }
 
-function Show_Slide () {
-	const Slide = document.getElementsByClassName('Slide'); 
-	
-	if(Current_Slide >= Slide.length) { 
-		Current_Slide = 0;
-	}
-	if (Current_Slide < 0) {
-		Current_Slide = Slide.length-1;
-	}
-	for (let i = 0; i < Slide.length; i++) {
-		Slide[i].style.display = "none";
-		on_Title_Slide_Click_Open_Modal[i].style.color = "rgb(255,255,255)"; // change to default color 
-	}
-	$(Slide[Current_Slide]).fadeIn("fast");
+function Show_Slide() {
+    const Slide = document.getElementsByClassName("Slide");
+
+    if (Current_Slide >= Slide.length) {
+        Current_Slide = 0;
+    }
+    if (Current_Slide < 0) {
+        Current_Slide = Slide.length - 1;
+    }
+    for (let i = 0; i < Slide.length; i++) {
+        Slide[i].style.display = "none";
+        on_Title_Slide_Click_Open_Modal[i].style.color = "rgb(255,255,255)"; // change to default color
+    }
+    $(Slide[Current_Slide]).fadeIn("fast");
 }
 
-const modal_alert = document.getElementsByClassName("modal-alert"); 
+const modal_alert = document.getElementsByClassName("modal-alert");
 const btn_close_modal = document.getElementsByClassName("btn-close");
 
 for (let index = 0; index < on_Title_Slide_Click_Open_Modal.length; index++) {
-	on_Title_Slide_Click_Open_Modal[index].addEventListener("click", (event) => {
-		event.target.style.color = "rgb(34, 34, 34)"; //Click on title slide change color
-		$(modal_alert[index]).fadeIn("medium").css({
-			display: "flex"
-		}); // show modal
-	});
-	
-	btn_close_modal[index].addEventListener("click", () => { 
-		$(modal_alert[index]).fadeOut("medium"); //hide when click on btn close
-	});
-	
+    on_Title_Slide_Click_Open_Modal[index].addEventListener("click", event => {
+        event.target.style.color = "rgb(34, 34, 34)"; //Click on title slide change color
+        $(modal_alert[index])
+            .fadeIn("medium")
+            .css({
+                display: "flex"
+            }); // show modal
+    });
+
+    btn_close_modal[index].addEventListener("click", () => {
+        $(modal_alert[index]).fadeOut("medium"); //hide when click on btn close
+    });
 }
